4e6b4457244b34cc7356ab9a3fef48774add0bd2
EL MAHDI BOULLOUL <boulloul.123@gmail.com>
2024-06-14 15:37:42 +0100

    .

--- src/app/components/apps/clients/modal/add-client/add-client.component.ts
+++ src/app/components/apps/clients/modal/add-client/add-client.component.ts
@@ -103,7 +103,7 @@ export class AddClientComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    console.log("addClient.ngOnInit");
+    console.log("addClient.ngOnInit...");
     this.getClients();
   }
   getClients() {
