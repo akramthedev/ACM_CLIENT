920ea7cabd3c4881b8a230f29fcfe6ff356a9f9b
akramthedev <seasonedwebdev@gmail.com>
2025-02-08 15:19:21 +0100

    Last Version Done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   CLIENT_ID : "267508651605-2vqqep29h97uef9tt7ahis82dskjsm1r.apps.googleusercontent.com",
   API_KEY : 'AIzaSyBhI34z9rSK7S-rfmngJ1nmb48zfb5nUz8',
   CLIENT_SECRET :"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",
+  // change it when production 
 
   keycloak: {
     // serverUrl: 'http://localhost:8080',
